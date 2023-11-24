3019131b0f2febfe6e3973f3305a4a95909fbe00
Aziz <bhsaziz408@gmail.com>
2023-11-24 20:46:18 +0100

    2nd ecent 1st cond

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ async def on_ready():
     print('{0.user} is here!'.format(client))
 
 @client.event
+async def on_message(msg):
+    if msg.author == client.user:
+        return
+    
+    if msg.content.startswith('!hello'):
