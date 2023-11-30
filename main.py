123990caab8e2a90a2cbf44184f6dd8d8ea6bbc3
Aziz <bhsaziz408@gmail.com>
2023-11-30 22:31:39 +0100

    Removed second bot event

--- main.py
+++ main.py
@@ -18,41 +18,42 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print('{0.user} is logged in!'.format(bot))
 
-@bot.event
-async def on_message(msg):
-    if msg.author == bot.user:
-        return
-    
-    if msg.content.startswith('!hello'):
-        await msg.channel.send('Hello sir!')
-
 @bot.command()
 async def weather(ctx: commands.Context, *, city):
-    url = "http://api.weatherapi.com/v1/current.json" 
+    print("Weather command invoked with city:", city)
+
+    url = "http://api.weatherapi.com/v1/current.json"
     params = {
         "key": key,
         "q": city
     }
-    
-    async with aiohttp.ClientSession() as session: 
+
+    async with aiohttp.ClientSession() as session:
         async with session.get(url, params=params) as res:
-           data = await res.json()
+            data = await res.json()
+            
+            print("API Response:", data)  # Add this line for debugging
 
-           location = data["location"]["name"]
-           temp_c = data["current"]["temp_c"]
-           temp_f = data["current"]["temp_f"]
-           humidity = data["current"]["humidity"]
-           wind_kph = data["current"]["wind_kph"]
-           condition = data["current"]["condition"]["text"]
-           image_url = "http:" + data["current"]["condition"]["icon"]
+            location = data.get("location", {}).get("name")
+            if not location:
+                print("Error: Location not found in API response.")
+                await ctx.send("Error: Location not found.")
+                return
 
-           embed = discord.Embed(title=f"weather for {location}", description=f"The condition in {location} is {condition}")
-           embed.add_field(name="Temperature", value=f"C: {temp_c} | F: {temp_f}")
-           embed.add_field(name="Humidity", value=f"{humidity}")
-           embed.add_field(name="Wind Speeds", value=f"{wind_kph}")
-           embed.set_thumbnail(url=image_url)
+            temp_c = data.get("current", {}).get("temp_c")
+            temp_f = data.get("current", {}).get("temp_f")
+            humidity = data.get("current", {}).get("humidity")
+            wind_kph = data.get("current", {}).get("wind_kph")
+            condition = data.get("current", {}).get("condition", {}).get("text")
+            image_url = "http:" + data.get("current", {}).get("condition", {}).get("icon")
 
-           await ctx.send(embed=embed)
+            embed = discord.Embed(title=f"Weather for {location}", description=f"The condition in {location} is {condition}")
+            embed.add_field(name="Temperature", value=f"C: {temp_c} | F: {temp_f}")
+            embed.add_field(name="Humidity", value=f"{humidity}")
+            embed.add_field(name="Wind Speeds", value=f"{wind_kph}")
+            embed.set_thumbnail(url=image_url)
+
+            await ctx.send(embed=embed)
 
 
 bot.run(token)
